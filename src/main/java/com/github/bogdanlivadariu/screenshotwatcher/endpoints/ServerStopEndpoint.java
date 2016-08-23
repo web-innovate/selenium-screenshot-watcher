4c6a389e9489f6b87f218af27926e498f6619b5e
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2016-08-23 12:56:40 +0300

    prevend shuting down heroku instance :)

--- src/main/java/com/github/bogdanlivadariu/screenshotwatcher/endpoints/ServerStopEndpoint.java
+++ src/main/java/com/github/bogdanlivadariu/screenshotwatcher/endpoints/ServerStopEndpoint.java
@@ -2,18 +2,29 @@ package com.github.bogdanlivadariu.screenshotwatcher.endpoints;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.Request;
 
 import com.github.bogdanlivadariu.screenshotwatcher.Main;
 import com.github.bogdanlivadariu.screenshotwatcher.util.EndpointUtil;
+import com.github.bogdanlivadariu.screenshotwatcher.util.EnvironmentUtil;
+import com.mongodb.util.JSON;
 
 @Path("/stop")
 public class ServerStopEndpoint {
     @POST
-    public void post(@Context Request request) throws InterruptedException {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response post(@Context Request request) throws InterruptedException {
         EndpointUtil.printClientInfo(request);
+        if ("heroku".equalsIgnoreCase(EnvironmentUtil.getEnvironmentName())) {
+            return Response.status(403)
+                .entity(JSON.serialize("App NOT is going down, just because you want so :)")).build();
+        }
         Main.SERVER.shutdownNow();
+        return null;
     }
 }
